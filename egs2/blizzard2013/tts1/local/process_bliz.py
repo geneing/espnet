db055062ba5370c058bde4faee428867f01998a1
Eugene Ingerman <eugene.ingerman@thermofisher.com>
2020-11-09 23:33:02 -0800

    Rename script

---
+++ egs2/blizzard2013/tts1/local/process_bliz.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+
+import os
+import argparse
+import csv
+
+
+def read_data( source_dir ):
+    utt=[]
+    str=[]
+    fpath=[]
+    with open(os.path.join(source_dir, 'all.txt'), 'r') as f:
+        reader = csv.reader(f, delimiter='|')
+        max_rows = 30000
+        for row in reader:
+            utt.append(row[0])
+            fpath.append(os.path.join(source_dir, row[1]))
+            str.append(row[2])
+            max_rows-=1
+            if max_rows<=0:
+                break
+    return (utt,fpath,str)
+
+def save_data(a, utt, fpath, str):
+    with open(a.text, 'w') as f:
+        for u, s in zip(utt, str):
+            f.write('%s %s\n'%(u,s))
+
+    with open(a.scp, 'w') as f:
+        for u, fp in zip(utt, fpath):
+            f.write('%s %s\n' % (u, fp))
+
+    with open(a.spk2utt, 'w') as f:
+        f.write("LJ ")
+        for u in utt:
+            f.write('%s '%(u))
+        f.write("\n")
+
+    with open(a.utt2spk, 'w') as f:
+        for u in utt:
+            f.write('%s LJ\n' % (u))
+
+
+def main():
+    print('Initializing Training Process..')
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--scp', default="data/train/wav.scp")
+    parser.add_argument('--utt2spk', default='data/train/utt2spk')
+    parser.add_argument('--spk2utt', default='data/train/spk2utt')
+    parser.add_argument('--text', default='data/train/text')
+    parser.add_argument('--source-dir', default='/mnt/local/TrainingData/Blizzard2013_Aligned/')
+
+    a = parser.parse_args()
+
+    (utt, fpath, str) = read_data(a.source_dir)
+    save_data(a, utt, fpath, str)
+
+if __name__ == '__main__':
+    main()
